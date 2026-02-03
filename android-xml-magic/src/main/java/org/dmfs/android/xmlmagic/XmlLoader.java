c191fb0a4079a6fe027048200794f26203585cd4
nosp7m <13512273+nosp7m@users.noreply.github.com>
2026-02-03 21:25:12 +0100

    Add prominent notices to "android-xml-magic" and "androidcarrot".

--- android-xml-magic/src/main/java/org/dmfs/android/xmlmagic/XmlLoader.java
+++ android-xml-magic/src/main/java/org/dmfs/android/xmlmagic/XmlLoader.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * This file has been changed from the original.
  */
 
 package org.dmfs.android.xmlmagic;
